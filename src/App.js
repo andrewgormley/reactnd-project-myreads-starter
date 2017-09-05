21490e7c13604e3f00540e7bddeee7c91b52f6a3
Andrew Gormley <andrew.gormley@crowdcube.com>
2017-09-05 17:22:12 +0100

    pulling data from the books api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 import BookList from './BookList.js'
 import BookSearch from './BookSearch.js'
 
 class BooksApp extends React.Component {
   state = {
-
+    books: []
+  }
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books })
+    })
   }
 
   render() {
