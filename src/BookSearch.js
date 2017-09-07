9493fb081a26d2ec89a079344c19c642a9e75406
Andrew Gormley <andrew.gormley@crowdcube.com>
2017-09-07 10:49:57 +0100

    feat: Added search functionality to search page

--- src/BookSearch.js
+++ src/BookSearch.js
@@ -1,31 +1,62 @@
 import React, { Component } from 'react';
+import * as BooksAPI from './BooksAPI'
 import { Link } from 'react-router-dom'
 import Book from './Book.js'
 
 class BookSearch extends Component {
 
+  state = {
+    query: '',
+    books: []
+  }
+
+  updateQuery =(query) => {
+    this.setState({
+      query
+  })
+
+  BooksAPI.search(query,25).then(results => {
+      if(results.length > 0) {
+        this.setState({
+          books:results
+        })
+      } else {
+        this.setState({
+          books:[]
+        })
+      }
+    })
+  }
+
   render() {
 
     return (
+
       <div className="search-books">
         <div className="search-books-bar">
           <Link to='/' className="close-search">Close</Link>
           <div className="search-books-input-wrapper">
-            {/*
-              NOTES: The search from BooksAPI is limited to a particular set of search terms.
-              You can find these search terms here:
-              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-              you don't find a specific author or title. Every search is limited by search terms.
-            */}
-            <input type="text" placeholder="Search by title or author"/>
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={this.state.query}
+              onChange={(event) => this.updateQuery(event.target.value)}
+            />
 
           </div>
         </div>
         <div className="search-books-results">
           <ol className="books-grid">
-
+            {this.state.books.length ? (
+               this.state.books.map((book) => {
+                return (
+                  <li key={book.id}>
+                     <Book
+                        book={book}
+                      />
+                  </li>)
+                })
+             ):<div>Currently showing no results</div>}
           </ol>
         </div>
       </div>
